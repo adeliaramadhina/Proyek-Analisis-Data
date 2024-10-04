907a3616d4c048bf11cb4c9cbde2804970c4d567
adeliaramadhina <adeliaramadhinara@gmail.com>
2024-10-05 00:40:37 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 
-sns.set(style='dark')
+sns.set(style='light')
 
 # Helper function yang dibutuhkan untuk menyiapkan berbagai dataframe
 
